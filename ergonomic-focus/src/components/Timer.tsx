064f8cea6bf6f5edc3084c15cc30b69efcb071d6
root <root@lnb028553bhz.ciandt.global>
2021-02-26 02:48:39 -0300

    Timer developed

--- ergonomic-focus/src/components/Timer.tsx
+++ ergonomic-focus/src/components/Timer.tsx
@@ -1,48 +1,75 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../styles/components/timer.css";
 import "../styles/components/button.css";
 import { ReactComponent as RightArrow } from "../assets/images/right-arrow-icon.svg";
 import { Button } from "./Button";
 
+
 export const Timer = () => {
-  function getTime() {
-    const now = new Date();
 
-    const hours = now.getHours();
-    const minutes = now.getMinutes();
-    const seconds = now.getSeconds();
 
-    console.log(hours + ":" + minutes + ":" + seconds);
-  }
+const now = new Date();
+
+const hours = now.getHours();
+const minutes = now.getMinutes();
+const seconds = now.getSeconds();
+
+const [timeSecond, setTimeSecond] = useState(seconds)
+const [active, setActive] = useState(false)
+
+const [hourLeft, hourRight] = String(hours)
+    .padStart(2, '0')
+    .split('')
+const [minuteLeft, minuteRight] = String(minutes)
+    .padStart(2, '0')
+    .split('')
+const [secondLeft, secondRight] = String(seconds)
+    .padStart(2, '0')
+    .split('')
+
+
+function startCoutDown() {
+    setActive(true);
+}
+
+useEffect(() => {
+
+    if (active && timeSecond < 59) {
+        setInterval(() => {
+            setTimeSecond(timeSecond + 1)
+        }, 1000)
+    }
+
+}, [active, timeSecond])
 
   return (
     <div>
       <div className="timer-container">
         <div className="container-numbers">
-          <div className="number">0</div>
+          <div className="number">{hourLeft}</div>
           <div className="divisor" />
-          <div className="number">0</div>
+          <div className="number">{hourRight}</div>
         </div>
         <div className="container-middle">
           <div className="number">:</div>
         </div>
         <div className="container-numbers">
-          <div className="number">0</div>
+          <div className="number">{minuteLeft}</div>
           <div className="divisor" />
-          <div className="number">0</div>
+          <div className="number">{minuteRight}</div>
         </div>
         <div className="container-middle">
           <div className="number">:</div>
         </div>
         <div className="container-numbers">
-          <div className="number">0</div>
+          <div className="number">{secondLeft}</div>
           <div className="divisor" />
-          <div className="number">0</div>
+          <div className="number">{secondRight}</div>
         </div>
       </div>
 
       <Button
-        onClick={getTime}
+        onClick={startCoutDown}
         color="blue"
         text="Iniciar um ciclo"
         icon={<RightArrow />}
